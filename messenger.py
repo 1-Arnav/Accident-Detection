b9b49c3f517a8696d0f1b49b637db9ab466b456d
1-Arnav <90562006+1-Arnav@users.noreply.github.com>
2024-04-05 12:00:25 +0530

    Update messenger.py

--- messenger.py
+++ messenger.py
@@ -1,7 +1,7 @@
 from twilio.rest import Client
 
 account_sid = 'AC5eb73b8b89d51b0c5bde9d0feb941768'
-auth_token = 'a8c43f6d22e546d744e03652f8c34f7a'
+auth_token = 'e3dc247165172cb7d6b10ae04323c115'
 twilio_phone_number = '+14066377357'
 recipient_phone_number = '+919325425933'
 
