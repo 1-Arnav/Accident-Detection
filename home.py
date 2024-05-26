6122385f6f97657a64cd899ecdb4a5723654c97b
1-Arnav <90562006+1-Arnav@users.noreply.github.com>
2024-05-26 14:31:57 +0530

    Updated home.py for version 2
    Changes in gui and functionality

--- home.py
+++ home.py
@@ -1,42 +1,77 @@
 import streamlit as st
 from predictor import predict
-from imgChooser import load_images, img_option
+from PIL import Image
+from streamlit_image_select import image_select
+from pathlib import Path
+from io import BytesIO
+import glob
+from vidPredictor import vid
 
 st.set_page_config(page_title="Accident Detecton", page_icon= ":collision:", layout="centered")
 st.title(" Accident Detection using machine learning")
 st.divider()
 
 
-uploaded_file = st.file_uploader("Upload your file here...", type=['png', 'jpeg', 'jpg'])
 
+def load_options():
+    options =[]
+    addresses =glob.glob('options/*')
+    for file in addresses:
+        file = file.replace("\\", "/")
+        parts = file.split('/')
+        if parts[-1] not in options:
+            options.append(parts[-1])
+    option = dict(zip(options, addresses))
+    return option
+
+def vid_op(f):
+    label, image =vid(f)
+ 
+    col1, col2 = st.columns(2)
+    col2.markdown("###")
+    with col1:
+        im =image_select(label="", captions=label , images=image, use_container_width=True, index=0, return_value="index")
+    with col2:
+        with st.container(border=True):
+            st.write(label[im])
+            st.image(image[im])
+
+def img_op(f):
+    st.write(predict(Image.open(f).resize((250,250))))
+    st.image(f)
+
+
+
+uploaded_file = st.file_uploader("Upload your file here...", type=['png', 'jpeg', 'jpg', 'mp4'])
 try:
-    st.write(predict(uploaded_file))
+    if Path(uploaded_file.name).suffix == '.mp4':
+        vid_op(uploaded_file)  #video function call
+    else:
+        img_op(uploaded_file)
 except:
     st.warning("Please upload a file")
 
-if uploaded_file is not None:
-    st.image(uploaded_file)
 
 st.markdown('#')
 st.subheader("OR Choose from below")
 st.markdown('#')
 
-col1, col2 = st.columns(2)
-col2.markdown("###")
 
-with col1:
-    img_files, manuscripts = load_images()
-    view_manuscripts = st.multiselect("Select Folder(s)", manuscripts, key=1)
-    try:
-        im =img_option(img_files, view_manuscripts)
-    except:
-        col2.warning("Please choose a folder")
-    # st.write(im)
-
-with col2:
-    with st.container(border=True):
-        try:
-            st.write(predict(im))
-            st.image(im)
-        except:
-            pass
+option = load_options()
+view_options = st.selectbox("Select your option", option.keys(),index=None)
+try:
+    if view_options[-3:] == 'mp4':
+        st.video(option[view_options])
+        with open(option[view_options], '+rb') as fh:
+            buf = BytesIO(fh.read())
+        vid_op(buf)
+    else:
+        a = glob.glob(option[view_options]+'/*')
+        col1, col2 = st.columns(2)
+        col2.markdown("###")
+        with col1:
+            im =image_select(label="select", images=a, use_container_width=False, index=-1)
+        with col2:
+            img_op(im)
+except:
+    st.warning("Please choose a file")
